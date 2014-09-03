d0defd01283dcb84ba777860a1c022255d42e4d7
brycesandlund <bcsandlund@gmail.com>
2014-09-03 15:43:10 -0700

    Added Hungarian Algorithm
    Thanks Stanford ACM notebook!

--- TestProject/TestProject/CodeFunctions.cpp
+++ TestProject/TestProject/CodeFunctions.cpp
@@ -37,6 +37,8 @@ typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ii> vii;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
 
 /*******************************************
 ***************data structures**************
@@ -291,6 +293,125 @@ int BipartiteMatching(const vvi &w, vi &mr, vi &mc) {
     if (FindMatch(i, w, mr, mc, seen)) ct++;
   }
   return ct;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Min cost bipartite matching via shortest augmenting paths
+//
+// This is an O(n^3) implementation of a shortest augmenting path
+// algorithm for finding min cost perfect matchings in dense
+// graphs.  In practice, it solves 1000x1000 problems in around 1
+// second.
+//
+//   cost[i][j] = cost for pairing left node i with right node j
+//   Lmate[i] = index of right node that left node i pairs with
+//   Rmate[j] = index of left node that right node j pairs with
+//
+// The values in cost[i][j] may be positive or negative.  To perform
+// maximization, simply negate the cost[][] matrix.
+///////////////////////////////////////////////////////////////////////////
+
+double MinCostMatching(const vvd &cost, vi &Lmate, vi &Rmate) {
+  int n = int(cost.size());
+
+  // construct dual feasible solution
+  vd u(n);
+  vd v(n);
+  for (int i = 0; i < n; i++) {
+    u[i] = cost[i][0];
+    for (int j = 1; j < n; j++) u[i] = min(u[i], cost[i][j]);
+  }
+  for (int j = 0; j < n; j++) {
+    v[j] = cost[0][j] - u[0];
+    for (int i = 1; i < n; i++) v[j] = min(v[j], cost[i][j] - u[i]);
+  }
+  
+  // construct primal solution satisfying complementary slackness
+  Lmate = vi(n, -1);
+  Rmate = vi(n, -1);
+  int mated = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (Rmate[j] != -1) continue;
+      if (fabs(cost[i][j] - u[i] - v[j]) < 1e-10) {
+	Lmate[i] = j;
+	Rmate[j] = i;
+	mated++;
+	break;
+      }
+    }
+  }
+  
+  vd dist(n);
+  vi dad(n);
+  vi seen(n);
+  
+  // repeat until primal solution is feasible
+  while (mated < n) {
+    
+    // find an unmatched left node
+    int s = 0;
+    while (Lmate[s] != -1) s++;
+    
+    // initialize Dijkstra
+    fill(dad.begin(), dad.end(), -1);
+    fill(seen.begin(), seen.end(), 0);
+    for (int k = 0; k < n; k++) 
+      dist[k] = cost[s][k] - u[s] - v[k];
+    
+    int j = 0;
+    while (true) {
+      
+      // find closest
+      j = -1;
+      for (int k = 0; k < n; k++) {
+	if (seen[k]) continue;
+	if (j == -1 || dist[k] < dist[j]) j = k;
+      }
+      seen[j] = 1;
+      
+      // termination condition
+      if (Rmate[j] == -1) break;
+      
+      // relax neighbors
+      const int i = Rmate[j];
+      for (int k = 0; k < n; k++) {
+	if (seen[k]) continue;
+	const double new_dist = dist[j] + cost[i][k] - u[i] - v[k];
+	if (dist[k] > new_dist) {
+	  dist[k] = new_dist;
+	  dad[k] = j;
+	}
+      }
+    }
+    
+    // update dual variables
+    for (int k = 0; k < n; k++) {
+      if (k == j || !seen[k]) continue;
+      const int i = Rmate[k];
+      v[k] += dist[k] - dist[j];
+      u[i] -= dist[k] - dist[j];
+    }
+    u[s] += dist[j];
+    
+    // augment along path
+    while (dad[j] >= 0) {
+      const int d = dad[j];
+      Rmate[j] = Rmate[d];
+      Lmate[Rmate[j]] = j;
+      j = d;
+    }
+    Rmate[j] = s;
+    Lmate[s] = j;
+    
+    mated++;
+  }
+  
+  double value = 0;
+  for (int i = 0; i < n; i++)
+    value += cost[i][Lmate[i]];
+  
+  return value;
 }
 
 /*******************************************
@@ -784,20 +905,20 @@ vector<int> primeFactorization(int n, vector<int> &primes)
 }
 
 int main() {
-  vvi adjMatrix;
-  vi row0;
+  vvd adjMatrix;
+  vd row0;
   row0.push_back(3);
   row0.push_back(3);
   row0.push_back(3);
   adjMatrix.push_back(row0);
 
-  vi row1;
+  vd row1;
   row1.push_back(3);
   row1.push_back(2);
   row1.push_back(3);
   adjMatrix.push_back(row1);
 
-  vi row2;
+  vd row2;
   row2.push_back(3);
   row2.push_back(3);
   row2.push_back(2);
@@ -805,7 +926,7 @@ int main() {
 
   vi rows(3, -1), cols(3, -1);
 
-  cout << BipartiteMatching(adjMatrix, rows, cols) << endl;
+  cout << MinCostMatching(adjMatrix, rows, cols) << endl;
   cout << rows << endl;
   cout << cols << endl;
 
